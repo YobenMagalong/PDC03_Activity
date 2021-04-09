82f9464fba5af7f0b860ebd1476b62204f9be511
YobenMagalong <82088806+YobenMagalong@users.noreply.github.com>
2021-04-09 14:53:31 +0800

    Add files via upload

---
+++ Activity5.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity5 : ContentPage
+    {
+        public Activity5()
+        {
+            InitializeComponent();
+            lst.ItemsSource = new List<Contacts>()
+            {
+                new Contacts()
+                {
+                    Name = "Yoben Magalong", Num = "123123",
+
+                },
+                new Contacts()
+                {
+                    Name = "Yoben Magalong", Num = "123123",
+                },
+                new Contacts()
+                {
+                    Name = "Yoben Magalong", Num = "123123",
+                },
+            };
+        }
+    }
+}
